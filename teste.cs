9bc270b01c1ace1786340a2c2dc7fc555aebbfef
Ewerton Clozato <ewertonclozato@otmail.com>
2021-03-31 20:57:25 -0300

    primeiro commit

---
+++ teste.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Olá Mundo");
+        Console.WriteLine("Olá");
+        Console.ReadKey();
+    }
+    
+}
